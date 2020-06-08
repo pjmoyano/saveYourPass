5eec7a6d24e8253e4526f79859f3129a1ed37852
pjmoyano <josemariamoyano.ar@gmail.com>
2020-06-08 13:08:33 -0300

    Start from start.spring.io

---
+++ src/main/java/com/pp/password/PasswordApplication.java
@@ -0,0 +1,13 @@
+package com.pp.password;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordApplication.class, args);
+	}
+
+}
