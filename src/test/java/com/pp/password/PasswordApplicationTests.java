55845f184ed91c49515c24ef44625ad5f20713b6
pjmoyano <josemariamoyano.ar@gmail.com>
2020-06-10 10:29:17 -0300

    TDD practice - starting with methods

--- src/test/java/com/pp/password/PasswordApplicationTests.java
+++ src/test/java/com/pp/password/PasswordApplicationTests.java
@@ -1,13 +1,21 @@
 package com.pp.password;
 
+import com.pp.password.utils.Utils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest
 class PasswordApplicationTests {
 
+	/*
+	Test for check encryption and decryption methods working well
+	 */
 	@Test
-	void contextLoads() {
+	public void  testEncryption(){
+		assertThat(Utils.decrypt(Utils.encrypt("Test")),containsString("Test"));
 	}
 
 }
